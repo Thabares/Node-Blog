f506a3998923d90039a21d1fc389ef2502b42505
Thabares <thabares786@gmail.com>
2021-11-25 10:00:01 +0530

    user routing and post routing updated

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express();
 const env = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
 
 env.config();
 
@@ -11,5 +14,8 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 
+app.use("/api/auth", authRoute)
+app.use("/api/user", userRoute)
+app.use("/api/post", postRoute)
 
 app.listen("5000", () => console.log("Backend is running"))
---
+++ routes/users.js
@@ -0,0 +1,61 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
+
+// Update
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You can only update your account!");
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ userName: user.userName });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(104).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can only delete your account!");
+  }
+});
+
+// GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const {password, ...others} = user._doc
+    res.status(200).json(others)
+  } catch (err) {
+    res.status(500).json("User not found!");
+  }
+});
+
+module.exports = router;
